bc7b18d3d1cc4f5bbe7f871005896ee0ca6a66e1
Lisa <frolovaie9@gmail.com>
2024-07-19 09:50:58 +0100

    Created superuser with username 'admin'

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create views here
 def index(request):
-    return HttpResponse("You are at the polls index.")
+    return HttpResponse("Hello, world. You are at the polls index.")
